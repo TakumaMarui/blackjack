eb002c49c8e4f513e451457eff4039bd7087b65d
TakumaMarui <circle.i.014@gmail.com>
2021-07-29 12:21:31 +0900

    プロフィール画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'profiles/index'
   devise_for :users
   get 'games/index'
   get 'users/update'
   post 'users/update'
   root to: "games#index"
-  resources :users, only: [:index, :update]
+  resources :users, only: [:index, :update, :show]
 end
